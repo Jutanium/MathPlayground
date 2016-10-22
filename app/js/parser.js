db3a092b7a736eada018b6a0d7798c629d243c48
Dan Jutan <danjutan@gmail.com>
2016-10-22 13:13:38 -0400

    Make parser recognize incomplete operations, like *2 or - or (3)/

--- app/js/parser.js
+++ app/js/parser.js
@@ -5,36 +5,41 @@ export default class Parser {
     }
 
     parseAndCreate(text) {
-        const add = /(\d+)\+(\d+)/.exec(text);
-        const subtract = /(\d+)\-(\d+)/.exec(text);
-        const multiply = /(\d+)\*(\d+)/.exec(text);
-        const divide = /(\(\d+)\)\/(\(\d+)\)/.exec(text);
+        console.log(text);
+        const add = /(\d+)*\+(\d+)*/.exec(text);
+        const subtract = /(\d+)*\-(\d+)*/.exec(text);
+        const multiply = /(\d+)*\*(\d+)*/.exec(text);
+        const divide = /(?:\((\d+)\))*\/(?:\((\d+)\))*/.exec(text);
         const number = /(\d+)/.exec(text);
 
         if (add) {
-            this._controller.createAdd({
-                attachLeft: this._controller.createNumber(add[1]),
-                attachRight: this._controller.createNumber(add[2])
-            });
+            let params = {};
+            if (add[1])
+                params.attachLeft = this._controller.createNumber(add[1]);
+            if (add[2])
+                params.attachRight = this._controller.createNumber(add[2]);
+            this._controller.createAdd(params);
         } else if (subtract) {
-            this._controller.createSubtract({
-                attachLeft: this._controller.createNumber(subtract[1]),
-                attachRight: this._controller.createNumber(subtract[2])
-            });
+            let params = {};
+            if (subtract[1])
+                params.attachLeft = this._controller.createNumber(subtract[1]);
+            if (subtract[2])
+                params.attachRight = this._controller.createNumber(subtract[2]);
+            this._controller.createSubtract(params);
         } else if (multiply) {
-            this._controller.createMultiply({
-                attachLeft: this._controller.createNumber(multiply[1]),
-                attachRight: this._controller.createNumber(multiply[2])
-            });
+            let params = {};
+            if (multiply[1])
+                params.attachLeft = this._controller.createNumber(multiply[1]);
+            if (multiply[2])
+                params.attachRight = this._controller.createNumber(multiply[2]);
+            this._controller.createMultiply(params);
         } else if (divide) {
-            for (let i = 1; i < divide.length; i++) {
-                divide[i] = divide[i].substring(1);
-            }
-
-            this._controller.createDivide({
-                attachLeft: this._controller.createNumber(divide[1]),
-                attachRight: this._controller.createNumber(divide[2])
-            })
+            let params = {};
+            if (divide[1])
+                params.attachLeft = this._controller.createNumber(divide[1]);
+            if (divide[2])
+                params.attachRight = this._controller.createNumber(divide[2]);
+            this._controller.createDivide(params);
         } else if (number) {
             this._controller.createNumber(number[0]);
         }
